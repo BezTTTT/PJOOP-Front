c56f560fea9fa2a84404066ebee3df9e1fe03933
BezTTTT <Best_1614@hotmail.com>
2023-03-15 15:40:54 +0700

    try on 4 div UI components

--- src/components/ingame/PlayMenu.jsx
+++ src/components/ingame/PlayMenu.jsx
@@ -5,16 +5,25 @@ import "./SubmitStyles.css";
 export default class App extends React.Component {
   render() {
     return (
-      <div className="grid-container"> 
-      
-        <div><Canvas /></div>
+      <div className="grid-container">
+        <div>TIMER</div>
+        <div className="block">
+          <label>Player1</label>
+        </div>
+        <div className="block">
+          <label>Player2</label>
+        </div>
+        <div className="block">
+          <label>Budget</label>
+        </div>
+        <div>
+          <Canvas />
+        </div>
         <div className="rower">
           <textarea className="textArea"></textarea>
           <button className="submitButton">plan and start</button>
         </div>
-       
-    </div>
-     
+      </div>
     );
   }
 }
