f1e0d43ab8dd92806a32902e9db5a647529b8d35
BezTTTT <Best_1614@hotmail.com>
2023-03-15 19:18:25 +0700

    button config , add round notification

--- src/components/ingame/PlayMenu.jsx
+++ src/components/ingame/PlayMenu.jsx
@@ -5,43 +5,31 @@ import "./SubmitStyles.css";
 export default class App extends React.Component {
   render() {
     return (
-
       <div className="center">
+        <div className="grid-container">
+          <div className="leftrower">
+            <div>TIMER</div>
+            <div className="block">
+              <label>Player1</label>
+              <div className="block">
+                <label>Player2</label>
+              </div>
+            </div>
+            <div className="block">
+              <label>Budget</label>
+            </div>
+          </div>
 
-<div className="grid-container">
-      <div className="leftrower" >
-        <div>TIMER</div>
-
-        <div className="block">
-          <label>Player1</label>
-          <div className="block">
-          <label>Player2</label>
-        </div>
-        </div>
-
-        
-
-        <div className="block">
-          <label>Budget</label>
+          <div>
+            <h1>Round xx</h1>
+            <Canvas />
+          </div>
+          <div className="rower">
+            <textarea className="textArea"></textarea>
+            <button className="submitButton">plan and start</button>
+          </div>
         </div>
       </div>
-     
-
-        <div>
-          <Canvas />
-        </div>
-        <div className="rower">
-          <textarea className="textArea"></textarea>
-          <button className="submitButton">plan and start</button>
-        </div>
-
-       
-
-      </div>
-
-      </div>
-     
-      
     );
   }
 }
