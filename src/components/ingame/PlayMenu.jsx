7c1ccb2b1a124cbb6854a3f04d1e6567acfd5ac2
Ixac <ixacarrrows@gmail.com>
2023-03-15 17:44:32 +0700

    fix grid

--- src/components/ingame/PlayMenu.jsx
+++ src/components/ingame/PlayMenu.jsx
@@ -5,17 +5,28 @@ import "./SubmitStyles.css";
 export default class App extends React.Component {
   render() {
     return (
-      <div className="grid-container">
+
+      <div className="center">
+
+<div className="grid-container">
+      <div className="leftrower" >
         <div>TIMER</div>
+
         <div className="block">
           <label>Player1</label>
-        </div>
-        <div className="block">
+          <div className="block">
           <label>Player2</label>
         </div>
+        </div>
+
+        
+
         <div className="block">
           <label>Budget</label>
         </div>
+      </div>
+     
+
         <div>
           <Canvas />
         </div>
@@ -23,7 +34,14 @@ export default class App extends React.Component {
           <textarea className="textArea"></textarea>
           <button className="submitButton">plan and start</button>
         </div>
+
+       
+
       </div>
+
+      </div>
+     
+      
     );
   }
 }
