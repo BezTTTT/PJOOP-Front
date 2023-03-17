420a7ab557bc8b1cd147e8c7486fa87ebf26504e
BezTTTT <Best_1614@hotmail.com>
2023-03-17 15:47:10 +0700

    change playMenu to function

--- src/components/ingame/PlayMenu.jsx
+++ src/components/ingame/PlayMenu.jsx
@@ -2,34 +2,34 @@ import Canvas from "../Canvas";
 import React from "react";
 import "./SubmitStyles.css";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="center">
-        <div className="grid-container">
-          <div className="leftrower">
-            <div>TIMER</div>
+function PlayMenu() {
+  return (
+    <div className="center">
+      <div className="grid-container">
+        <div className="leftrower">
+          <div>TIMER</div>
+          <div className="block">
+            <label>Player1</label>
             <div className="block">
-              <label>Player1</label>
-              <div className="block">
-                <label>Player2</label>
-              </div>
-            </div>
-            <div className="block">
-              <label>Budget</label>
+              <label>Player2</label>
             </div>
           </div>
-
-          <div>
-            <h1>Round xx</h1>
-            <Canvas />
-          </div>
-          <div className="rower">
-            <textarea className="textArea"></textarea>
-            <button className="submitButton">plan and start</button>
+          <div className="block">
+            <label>Budget</label>
           </div>
         </div>
+
+        <div>
+          <h1>Round xx</h1>
+          <Canvas />
+        </div>
+        <div className="rower">
+          <textarea className="textArea"></textarea>
+          <button className="submitButton">plan and start</button>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default PlayMenu;
