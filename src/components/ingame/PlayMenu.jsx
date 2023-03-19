8153ee1ef8d2f6d6efd1b95cac9cbec0bb39f709
Ixac <ixacarrrows@gmail.com>
2023-03-19 21:18:23 +0700

    grid done

--- src/components/ingame/PlayMenu.jsx
+++ src/components/ingame/PlayMenu.jsx
@@ -6,29 +6,31 @@ import Timer from "./Timer";
 function PlayMenu() {
   return (
     <div className="center">
-      <div className="grid-container">
-        <div className="leftrower">
-          <div>
-            Timer : <Timer />
-          </div>
-          <div className="block">
-            <button>Player1</button>
+      <div className="gridROWROUND">
+         <h1 className="roundHeading">ROUND XXXX</h1>
+        <div className="grid-container">
+          <div className="leftrower">
+            <div>
+              Timer : <Timer />
+            </div>
             <div className="block">
-              <button>Player2</button>
+              <button>Player1</button>
+              <div className="block">
+                <button>Player2</button>
+              </div>
+            </div>
+            <div className="block">
+              <label>Budget</label>
             </div>
           </div>
-          <div className="block">
-            <label>Budget</label>
-          </div>
-        </div>
 
-        <div>
-          <h1>Round xx</h1>
-          <Canvas />
-        </div>
-        <div className="rower">
-          <textarea className="textArea"></textarea>
-          <button className="submitButton">plan and start</button>
+          <div>
+            <Canvas />
+          </div>
+          <div className="rower">
+            <textarea className="textArea"></textarea>
+            <button className="submitButton">plan and start</button>
+          </div>
         </div>
       </div>
     </div>
